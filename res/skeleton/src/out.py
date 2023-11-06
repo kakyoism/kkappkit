70eca04aba523ebbb438b0531fccf6e162ef970b
Beinan Li <li.beinan@gmail.com>
2023-11-06 09:33:55 -0600

    better docs

--- res/skeleton/src/out.py
+++ res/skeleton/src/out.py
@@ -1,11 +1,12 @@
-#
-# GENERATED: DO NOT EDIT
-#
 from dataclasses import dataclass
 import typing
+# ADD IMPORTS FOR CUSTOM TYPES
 
 
 @dataclass
 class Output:
+    """
+    - myProp: myType = myDefault
+    """
 # {{assign}}
     pass
