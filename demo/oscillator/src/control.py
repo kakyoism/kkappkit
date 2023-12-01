fb1bfbe518bd20e623694d39e2f704e377ea3da0
Bin Luo <bin.luo@mihoyo.com>
2023-12-01 15:58:11 +0800

    - added icon master - fixed: cross-platform path

--- demo/oscillator/src/control.py
+++ demo/oscillator/src/control.py
@@ -51,7 +51,7 @@ class ControllerImp:
             if not util.confirm(f'Missing user Csound script: {scpt}', 'Proceed to use default script? Otherwise switch to a different script and restart app', title='Warning'):
                 self.on_term(None)
                 return
-            scpt = osp.join(osp.dirname(__file__), '../res/tonegen.csd')
+            scpt = osp.abspath(f'{osp.dirname(__file__)}/../res/tonegen.csd')
             # refresh entry view
             self.controller.model['General']['Csound Script'][0] = scpt
             self.controller.reflect()
